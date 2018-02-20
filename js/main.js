b79df676466101ccf4b3c31fc13cfbf54df1221f
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2018-02-20 23:57:00 +0530

    fixed menu

--- js/main.js
+++ js/main.js
@@ -3,12 +3,10 @@ var menu = function()
 	var m = document.getElementById("menu");
 	m.style.left = "0";
 	m.style.zIndex = "1";
-	document.body.onclick = "closeMenu()";
 }
 
 var closeMenu = function()
 {
 	var m = document.getElementById("menu");
-	m.style.left = "-70%";
-	document.body.onclick = "";
+	m.style.left = "-80%";
 }
