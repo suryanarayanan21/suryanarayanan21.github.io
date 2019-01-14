3e71f3cac4e036062ead41c289ef4869b0b41575
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2019-01-15 01:01:25 +0530

    Added service worker
    Added service worker that caches the main html, css and javascript pages and adopts Progressive Web App technology

---
+++ sw.js
@@ -0,0 +1,28 @@
+var cacheName = 'nuggetCache';
+var files_to_cache = [
+  './index.html',
+  './CSS/main.css',
+  './JS/main.js'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(files_to_cache);
+    })
+  );
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
